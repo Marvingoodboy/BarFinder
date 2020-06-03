d7c821f6f32d5bf41768678a2bdd055ff8e4cb9a
marvingoodboy <marvingoodboy@gmail.com>
2020-06-03 21:13:36 +0300

    Refactoring

---
+++ BarFinder/CoordinateProcessor.cs
@@ -0,0 +1,73 @@
+﻿using BarFinder.Models;
+using NativeWifi;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+
+namespace BarFinder
+{
+    class CordinateProcessor
+    {
+        private const string GEOLOCATION_API_HOST = "https://api.mylnikov.org/geolocation/wifi?bssid=";
+        private const string BAR_DATABASE_PATH = @"C:\features.json";
+
+        private static Institutions DeserializeJsonFile()
+        {
+            string json = File.ReadAllText(BAR_DATABASE_PATH);
+            return JsonConvert.DeserializeObject<Institutions>(json);
+        }
+
+        private static List<Point> GetBarPoints(Institutions barCoordinates)
+        {
+            List<Point> point = new List<Point>();
+            foreach (Feature feature in barCoordinates.Features)
+            {
+                if (feature.Properties.Attributes.TypeObject.Equals("бар"))
+                {
+                    point.Add(new Point()
+                    {
+                        Name = feature.Properties.Attributes.Name,
+                        X = feature.Geometry.Coordinates[1],
+                        Y = feature.Geometry.Coordinates[0]
+                    });
+                }
+            }
+            return point;
+        }
+
+        private static MyLocation GetUserCoordinates()
+        {
+            string[] BSSID = null;
+            foreach (WlanClient.WlanInterface wlanInterface in new WlanClient().Interfaces)
+            {
+                foreach (Wlan.WlanBssEntry wlanBssEntry in wlanInterface.GetNetworkBssList())
+                {
+                    byte[] macAddr = wlanBssEntry.dot11Bssid;
+                    UInt32 macAddrLen = (uint)macAddr.Length;
+                    BSSID = new string[(int)macAddrLen];
+                    for (int i = 0; i < macAddrLen; i++)
+                    {
+                        BSSID[i] = macAddr[i].ToString("x2");
+                    }
+                }
+            }
+            string coodrinates = "";
+            WebRequest request = WebRequest.Create(GEOLOCATION_API_HOST + string.Join("", BSSID));
+            WebResponse response = request.GetResponse();
+            using (Stream stream = response.GetResponseStream())
+            {
+                using (StreamReader reader = new StreamReader(stream))
+                {
+                    coodrinates = reader.ReadLine();
+                }
+
+            }
+            response.Close();
+            return JsonConvert.DeserializeObject<MyLocation>(coodrinates);
+        }
+    }
+
+
+}
--- BarFinder/MapProcessor.cs
+++ BarFinder/MapProcessor.cs
@@ -2,33 +2,36 @@
 using GMap.NET;
 using GMap.NET.WindowsForms;
 using GMap.NET.WindowsForms.Markers;
-using NativeWifi;
-using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Device.Location;
 using System.Drawing;
-using System.IO;
-using System.Net;
 using System.Windows.Forms;
 
 namespace BarFinder
 {
     public partial class Form1 : Form
     {
-        private double userX = 0;
-        private double userY = 0;
-        private const string GEOLOCATION_API_HOST = "https://api.mylnikov.org/geolocation/wifi?bssid=";
+        private double userX;
+        private double userY;
+
+        private List<Point> barCoordinates;
 
         public Form1()
         {
             InitializeComponent();
+            MyLocation currentLocation = CoordinateProcessor.GetUserCoordinates();
+
+            this.userX = currentLocation.Data.Lat;
+            this.userY = currentLocation.Data.Lon;
+
+            Institutions institutions = CoordinateProcessor.DeserializeJsonFile();
+
+            this.barCoordinates = CoordinateProcessor.GetBarPoints(institutions);
         }
 
         private void gMapControl1_Load(object sender, EventArgs e)
         {
-
-            GetUserCoordinates();
             ////HardCode user coordinates
             //userX = 0;
             //userY = 0;
@@ -58,57 +61,8 @@ namespace BarFinder
 
             PutUserMarkerOnMap();
         }
-        private void GetUserCoordinates()
-        {
-            string[] BSSID = null;
-            foreach (WlanClient.WlanInterface wlanInterface in new WlanClient().Interfaces)
-            {
-                foreach (Wlan.WlanBssEntry wlanBssEntry in wlanInterface.GetNetworkBssList())
-                {
-                    byte[] macAddr = wlanBssEntry.dot11Bssid;
-                    UInt32 macAddrLen = (uint)macAddr.Length;
-                    BSSID = new string[(int)macAddrLen];
-                    for (int i = 0; i < macAddrLen; i++)
-                    {
-                        BSSID[i] = macAddr[i].ToString("x2");
-                    }
-                }
-            }
-            string coodrinates = "";
-            WebRequest request = WebRequest.Create(GEOLOCATION_API_HOST + string.Join("", BSSID));
-            WebResponse response = request.GetResponse();
-            using (Stream stream = response.GetResponseStream())
-            {
-                using (StreamReader reader = new StreamReader(stream))
-                {
-                    coodrinates = reader.ReadLine();
-                }
 
-            }
-            response.Close();
-            var test = JsonConvert.DeserializeObject<MyLocation>(coodrinates);
-            userX = test.Data.Lat;
-            userY = test.Data.Lon;
-        }
-        private List<Point> DeserializeJsonFile()
-        {
-            string json = File.ReadAllText(@"C:\features.json");
-            var bar = JsonConvert.DeserializeObject<Institutions>(json);
-            List<Point> point = new List<Point>();
-            foreach (Feature feature in bar.Features)
-            {
-                if (feature.Properties.Attributes.TypeObject.Equals("бар"))
-                {
-                    point.Add(new Point()
-                    {
-                        Name = feature.Properties.Attributes.Name,
-                        X = feature.Geometry.Coordinates[1],
-                        Y = feature.Geometry.Coordinates[0]
-                    });
-                }
-            }
-            return point;
-        }
+
         private void PutBarMarkersOnMap(List<Point> point)
         {
             GMapOverlay markersOverlay = new GMapOverlay("Bar");
